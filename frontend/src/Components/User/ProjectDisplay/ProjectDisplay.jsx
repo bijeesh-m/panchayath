dc3dfb948caede64ac6939689fe748e42d99ed2e
Bijeesh M <bijeeshh.m@gmail.com>
2025-02-24 01:47:16 +0530

    fix: fixed the bug on project display page

--- frontend/src/Components/User/ProjectDisplay/ProjectDisplay.jsx
+++ frontend/src/Components/User/ProjectDisplay/ProjectDisplay.jsx
@@ -1,57 +1,65 @@
 import React, { useEffect, useState } from "react";
 import "./ProjectDisplay.css";
 import { getAllProjects } from "../../../Services/userApi";
-import { Link } from "react-router-dom";
 
 function ProjectDisplay() {
-  const [project, setProject] = useState([]);
-  useEffect(() => {
-    getAllProjects().then((res) => {
-      console.log(res.data.data);
-      setProject(res.data.data);
-    });
-  }, []);
-  return (
-    <div>
-      <h4 className="m-4 ">Panchayath's Projects</h4>
-      <div
-        className="accordion container-fluid accordionMainDiv"
-        id="accordionPanelsStayOpenExample"
-      >
-        {project.map((value) => (
-          <div className="accordion-item myAccordion">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#panelsStayOpen-collapseOne"
-                aria-expanded="true"
-                aria-controls="panelsStayOpen-collapseOne"
-              >
-                {value.projectName}
-              </button>
-            </h2>
-            <div
-              id="panelsStayOpen-collapseOne"
-              className="accordion-collapse collapse show"
-            >
-             <span className="projectDate"><strong>Date : </strong>{value.Date}</span> 
-             <div className="accordion-body">
-          <div className="descriptionDiv">
-            <div className="projectImg"><img className="Pimage" src={`http://localhost:4000/Img/${value.projectImage}`}/></div>
-<div className="projectDesc">  {value.projectDescription}</div>
+    const [project, setProject] = useState([]);
 
-          </div>
-          <div className="website">Website : <Link>{value.website}</Link></div>
-           
-              </div>
+    useEffect(() => {
+        getAllProjects().then((res) => {
+            console.log(res.data.data);
+            setProject(res.data.data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h4 className="m-4">Panchayath's Projects</h4>
+            <div className="accordion container-fluid accordionMainDiv" id="accordionPanelsStayOpenExample">
+                {project.map((value, index) => {
+                    const uniqueId = `collapse-${index}`; // Generate a unique ID for each project
+                    return (
+                        <div className="accordion-item myAccordion" key={index}>
+                            <h2 className="accordion-header">
+                                <button
+                                    className="accordion-button"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target={`#${uniqueId}`} // Use unique ID
+                                    aria-expanded="false"
+                                    aria-controls={uniqueId}
+                                >
+                                    {value.projectName}
+                                </button>
+                            </h2>
+                            <div id={uniqueId} className="accordion-collapse project-container collapse">
+                                <div>
+                                    <img
+                                        className="Pimage"
+                                        src={`http://localhost:4000/Img/${value.projectImage}`}
+                                        alt="Project"
+                                    />
+                                </div>
+                                <div className="pro-dec">
+                                    <span className="projectDate">
+                                        <strong>Date : </strong>
+                                        {value.Date}
+                                    </span>
+                                    <div className="projectDesc">{value.projectDescription}</div>
+                                    <div className="website">
+                                        Website:{" "}
+                                        <a href={value.website} target="_blank" rel="noopener noreferrer">
+                                            {value.website}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default ProjectDisplay;
